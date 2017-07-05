18132a4647b1788de8ffd246f414af3e0cd04d86
freetryMyleft <632565746@qq.com>
2017-07-05 09:07:56 +0800

    新建Frgement包

---
+++ app/src/main/java/tarena/com/coolcartoon/CounnunityFrgement/Counnnunibean.java
@@ -0,0 +1,16 @@
+package tarena.com.coolcartoon.CounnunityFrgement;
+
+/**
+ * Created by tarena on 2017/7/4.
+ */
+
+public class Counnnunibean {
+    // 头像
+    // 昵称
+    //等级
+    //vip是否
+    //点赞数量
+    //阅读数量
+    //发布时间
+
+}
---
+++ app/src/main/java/tarena/com/coolcartoon/CounnunityFrgement/Toolbarutli.java
@@ -0,0 +1,9 @@
+package tarena.com.coolcartoon.CounnunityFrgement;
+
+/**
+ * Created by tarena on 2017/7/4.
+ */
+
+public class Toolbarutli {
+    //显示隐藏toolbar上的imgView
+}
